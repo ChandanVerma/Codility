ca9a098ae176870cb4fe19a9b95363270a83e80f
Chandan Verma <verma.chandan01@gmail.com>
2020-05-25 11:17:21 +0530

    100% result

--- binary_gap.py
+++ binary_gap.py
@@ -1,22 +1,21 @@
+### Correctness 80%
 def solution(N):
     """ This function first creats the interger to 
     binary representation and then calculates number 
     of binary gaps in the binary representation of N"""
-    binary_rep = []
-    one_index = []
-    binary_gap = []
-    while N >= 1:
-        remainder = N % 2
-        N = round(N / 2)
-        binary_rep.append(remainder)
-    binary_rep.reverse()
-    print(binary_rep)
-    one_index = [i for i, e in enumerate(binary_rep, 1) if e == 1]
-    if len(one_index) == 1:
-        return 'No Binary gaps'
-    binary_gap = max([j-i for i, j in zip(one_index[:-1], one_index[1:])])
-    binary_gap = binary_gap - 1
+    current_gap = 0
+    max_gap = 0
+    while (N > 0 and N%2 ==0):
+        N //= 2
+
+    while N > 0:
+        remainder = N%2
+        if remainder == 0:
+            current_gap += 1
+        elif remainder == 1:
+            max_gap = max(current_gap, max_gap)
+            current_gap = 0
+        
+        N //= 2
 
     return binary_gap
-
-binary_gap = solution(15)
